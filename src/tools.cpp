2e816d2764accd760e34ad6e1b1c01cd0ac2b4d7
Yihsun Ethan Cheng <33135703+YihsunEthanCheng@users.noreply.github.com>
2019-01-03 00:13:43 -0800

    Implemented rmse and Jacobian functions

--- src/tools.cpp
+++ src/tools.cpp
@@ -3,7 +3,7 @@
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
-using std::vector;
+using std::vector, std::cout, std::endl;
 
 Tools::Tools() {}
 
@@ -14,11 +14,48 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+
+   VectorXd rmse(ground_truth.size());
+   rmse = VectorXd::Zero(ground_truth.size());
+
+   if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
+      cout << "Invalid estimation or ground_truth data" << endl;
+      return rmse;
+   }
+
+   for (int i=0;i< ground_truth.size(); ++i) {
+      VectorXd d = estimations[i] - ground_truth[i];
+      rmse += VectorXd(d.array()*d.array());
+   }
+
+   rmse = rmse.array().sqrt() / ground_truth.size();
+
+   return rmse;
 }
 
+#define sqr(x) x*x
+
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+   
+   MatrixXd Jacobian(3,4);
+
+  // recover state parameters
+  float px2y2 = x_state(0) * x_state(0) + x_state(1) * x_state(1);
+  float sqrt_px2y2 = sqrt(px2y2);
+  float sqrt_px2y2_3 = sqrt(px2y2* px2y2* px2y2);
+
+  // check division by zero
+  if (fabs(px2y2) < 0.0001) {
+    cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+  } else{
+    // compute the Jacobian matrix
+    Jacobian << 
+       x_state(0)/sqrt_px2y2, x_state(1)/sqrt_px2y2, 0, 0,
+      -x_state(1)/px2y2, x_state(0)/px2y2, 0, 0,
+       x_state(1)*(x_state(2)*x_state(1) - x_state(3)*x_state(0))/sqrt_px2y2_3,
+       x_state(0)*(x_state(3)*x_state(0) - x_state(2)*x_state(1))/sqrt_px2y2_3,
+       x_state(0)/sqrt_px2y2, x_state(1)/sqrt_px2y2;
+   }
+
+  return Jacobian;
 }
